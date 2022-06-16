379c50e8ba255f46bb2a81a8d1f2aaf218c92371
Garibundo <71353278+Garibundo@users.noreply.github.com>
2022-06-16 14:26:25 -0500

    Add files via upload

---
+++ jscpt/Dress_Code.js
@@ -0,0 +1,21 @@
+function Funct1() {
+  document.getElementById('Title').innerHTML= "2nd Wisconsin";
+}
+function Funct2() {
+  document.getElementById('Title').innerHTML= "14th Brooklyn";
+}
+function Funct3() {
+  document.getElementById('Title').innerHTML= "2nd U.S Sharpshooters";
+}
+function Funct4() {
+  document.getElementById('Title').innerHTML= "19th Indiana";
+}
+function Funct5() {
+  document.getElementById('Title').innerHTML= "24th Michigan";
+}
+function Funct6() {
+  document.getElementById('Title').innerHTML= "1st Maryland Cavalry";
+}
+function Funct7() {
+  document.getElementById('Title').innerHTML= "4th U.S Battery";
+}
